cdbd4d8e42adc3d432d4d726c61320df28354126
Lucy402 <47191197+Lucy402@users.noreply.github.com>
2019-02-14 04:14:04 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("gintoki147")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("gintoki147© 2009-2015")]
+[assembly: AssemblyDescription("gintoki147")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyProduct("gPatcher")]
+[assembly: AssemblyTitle("gPatcher")]
+[assembly: AssemblyTrademark("gintoki147")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: Guid("3afb8a5e-3d74-4233-a294-15a73fdc004f")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
